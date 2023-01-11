f59ba5110c7305999fc658bce20ec7364376f3e1
minh-le0205 <minh.levunguyen@gmail.com>
2023-01-11 21:57:27 +0700

    Update quote API

--- app.js
+++ app.js
@@ -3,19 +3,31 @@ const text = document.querySelector(".card-text");
 const title = document.querySelector(".card-title");
 const copyButton = document.querySelector("#btn-copy");
 const speechButton = document.querySelector("#btn-speech");
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "aac23286b8msh7633f1dfeecfff8p15724bjsncc6d8e7fc34c",
+    "X-RapidAPI-Host": "quotes-by-api-ninjas.p.rapidapi.com",
+  },
+};
 
 async function randomQuote() {
-  const res = await fetch("https://quote-garden.herokuapp.com/api/v3/quotes");
+  const res = await fetch(
+    "https://quotes-by-api-ninjas.p.rapidapi.com/v1/quotes",
+    options
+  );
+
   const response = await res.json();
-  return response.data;
+  return response;
 }
 
 quoteButton.addEventListener("click", async function () {
   try {
     const randomIndex = Math.floor(Math.random() * 10);
     const quote = await randomQuote();
-    text.textContent = quote[randomIndex].quoteText;
-    title.textContent = "-" + quote[randomIndex].quoteAuthor;
+    console.log(quote[0]);
+    text.textContent = quote[0].quote;
+    title.textContent = "-" + quote[0].author;
   } catch (error) {
     swal({
       title: "Error",
